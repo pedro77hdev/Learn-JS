be5b8867fc08d582764b30dc7bcd341f87e6e57f
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-06-04 21:28:28 -0300

    Aprendendo JS - adição de imagens e funcionalidades nos botoes

--- jquery/script.js
+++ jquery/script.js
@@ -1,19 +1,22 @@
-$(document).ready(function () {
+$(document).ready(()=>{
     $("#rapaz").click(function () {
         var rapaz = new Audio("sons/rapaz.mp3")
         rapaz.play()
     })
-    $("#calma").click(function(){
+    $("#calma").click(()=>{
         var calma = new Audio("sons/calma.mp3")
         calma.play()
     })
-    $("#uepa").click(function(){
+    $("#uepa").click(()=>{
         var uepa = new Audio("sons/uepa.mp3")
         uepa.play()
     })
-    $("#elegosta").click(function(){
+    $("#elegosta").click(()=>{
         var elegosta = new Audio("sons/elegosta.mp3")
         elegosta.play()
     })
+    $("#olha").click(()=>{
+        var calma = new Audio("sons/olha.mp3")
+        calma.play()
+    })
 })
-
